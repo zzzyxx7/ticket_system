c1eba55e04a5cd2493ea5ac2fc0309d7911f1fd9
zzzyxx7 <3040202533@qq.com>
2025-12-16 22:34:41 +0800

    feat(*):安装Redis和Sa-token(但这个还没启用)

--- src/main/java/com/ticket/config/WebConfig.java
+++ src/main/java/com/ticket/config/WebConfig.java
@@ -3,12 +3,14 @@ package com.ticket.config;
 
 import com.ticket.interceptor.AuthInterceptor;
 import com.ticket.interceptor.JwtInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
